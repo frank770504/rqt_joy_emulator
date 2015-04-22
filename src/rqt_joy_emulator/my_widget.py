2792ca0c4cdc9df0f46bff5c6408f594db3dd2e8
otim <tim.oberhauser@gmail.com>
2015-04-22 16:32:09 +0200

    added field to enter namespace to publish joy msgs to. more compact button bindings using lambda

--- src/rqt_joy_emulator/my_widget.py
+++ src/rqt_joy_emulator/my_widget.py
@@ -5,6 +5,7 @@ import rospy
 import rospkg
 
 from python_qt_binding import loadUi
+from python_qt_binding.QtCore import SIGNAL
 from python_qt_binding.QtGui import QGraphicsView, QWidget
 
 from sensor_msgs.msg import Joy
@@ -29,19 +30,35 @@ class MyWidget(QWidget):
         style_sheet = ".QWidget { background: url(\"%s\"); }" % image_path
         self.widget.setStyleSheet(style_sheet)
 
-        self.aButton.clicked[bool].connect(self._handle_a_clicked)
-        self.bButton.clicked[bool].connect(self._handle_b_clicked)
-        self.xButton.clicked[bool].connect(self._handle_x_clicked)
-        self.yButton.clicked[bool].connect(self._handle_y_clicked)
+        # Press buttons.
+        self.aButton.pressed.connect(lambda: self.pressButton('A'))
+        self.bButton.pressed.connect(lambda: self.pressButton('B'))
+        self.xButton.pressed.connect(lambda: self.pressButton('X'))
+        self.yButton.pressed.connect(lambda: self.pressButton('Y'))
 
-        self.startButton.clicked[bool].connect(self._handle_start_clicked)
-        self.backButton.clicked[bool].connect(self._handle_back_clicked)
+        self.startButton.pressed.connect(lambda: self.pressButton('START'))
+        self.backButton.pressed.connect(lambda: self.pressButton('BACK'))
 
-        self.rbButton.clicked[bool].connect(self._handle_rb_clicked)
-        self.lbButton.clicked[bool].connect(self._handle_lb_clicked)
+        self.rbButton.pressed.connect(lambda: self.pressButton('RB'))
+        self.lbButton.pressed.connect(lambda: self.pressButton('LB'))
 
-        self.rjButton.clicked[bool].connect(self._handle_rj_clicked)
-        self.ljButton.clicked[bool].connect(self._handle_lj_clicked)
+        self.rjButton.pressed.connect(lambda: self.pressButton('RJ'))
+        self.ljButton.pressed.connect(lambda: self.pressButton('LJ'))
+
+        # Release buttons.
+        self.aButton.released.connect(lambda: self.releaseButton('A'))
+        self.bButton.released.connect(lambda: self.releaseButton('B'))
+        self.xButton.released.connect(lambda: self.releaseButton('X'))
+        self.yButton.released.connect(lambda: self.releaseButton('Y'))
+
+        self.startButton.released.connect(lambda: self.releaseButton('START'))
+        self.backButton.released.connect(lambda: self.releaseButton('BACK'))
+
+        self.rbButton.released.connect(lambda: self.releaseButton('RB'))
+        self.lbButton.released.connect(lambda: self.releaseButton('LB'))
+
+        self.rjButton.released.connect(lambda: self.releaseButton('RJ'))
+        self.ljButton.released.connect(lambda: self.releaseButton('LJ'))
 
         # Logitech Wired
         self.getButtonIdx = {'A':0,
@@ -58,7 +75,11 @@ class MyWidget(QWidget):
         self.joy_msg = Joy()
         self.joy_msg.axes = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         self.joy_msg.buttons = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        self.joy_pub = rospy.Publisher("/joy", Joy, queue_size = 100)
+        self.ns = ""
+        self.updatePublisher()
+
+        self.connect(self.namespaceLineEdit, SIGNAL("editingFinished()"),
+                     self.handleNamespaceChanged)
 
     def pressButton(self, button):
         self.joy_msg.buttons[self.getButtonIdx[button]] = 1
@@ -66,36 +87,20 @@ class MyWidget(QWidget):
         self.joy_pub.publish(self.joy_msg)
         time.sleep(0.1)
         # Release Button again.
+#         self.joy_msg.buttons[self.getButtonIdx[button]] = 0
+#         self.joy_msg.header.stamp = rospy.Time.now()
+#         self.joy_pub.publish(self.joy_msg)
+
+    def releaseButton(self, button):
         self.joy_msg.buttons[self.getButtonIdx[button]] = 0
         self.joy_msg.header.stamp = rospy.Time.now()
         self.joy_pub.publish(self.joy_msg)
 
-    def _handle_a_clicked(self):
-        self.pressButton('A')
+    def handleNamespaceChanged(self):
+        self.ns = str(self.namespaceLineEdit.text())
+        if not len(self.ns) == 0 and not self.ns[0] == "/":
+            self.ns = "/" + self.ns
+        self.updatePublisher()
 
-    def _handle_b_clicked(self):
-        self.pressButton('B')
-
-    def _handle_x_clicked(self):
-        self.pressButton('X')
-
-    def _handle_y_clicked(self):
-        self.pressButton('Y')
-
-    def _handle_start_clicked(self):
-        self.pressButton('START')
-
-    def _handle_back_clicked(self):
-        self.pressButton('BACK')
-
-    def _handle_rb_clicked(self):
-        self.pressButton('RB')
-
-    def _handle_lb_clicked(self):
-        self.pressButton('LB')
-
-    def _handle_rj_clicked(self):
-        self.pressButton('RJ')
-
-    def _handle_lj_clicked(self):
-        self.pressButton('LJ')
+    def updatePublisher(self):
+        self.joy_pub = rospy.Publisher(self.ns + "/joy", Joy, queue_size = 100)
